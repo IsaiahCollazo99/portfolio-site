95ecf7e0d1ce96152c1ab7925d3260173001d5fa
Isaiah <isaiahramoncollazo@gmail.com>
2020-05-02 11:06:15 -0400

    create home and projects page components and their css files

--- isaiah-collazo/src/App.js
+++ isaiah-collazo/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './App.css';
+import NavBar from './components/navBar/NavBar';
+import { Switch, Route } from 'react-router-dom';
+import Home from './components/home/Home';
+import Projects from './components/projects/Projects';
 
 function App() {
   return (
     <div className="App">
-      Isaiah Collazo
+      <NavBar />
+      <Switch>
+        <Route exact path={"/"}>
+          <Home />
+        </Route>
+
+        <Route exact path={"/projects"}>
+          <Projects />
+        </Route>
+      </Switch>
     </div>
   );
 }
