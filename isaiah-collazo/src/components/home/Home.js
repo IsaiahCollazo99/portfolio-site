40eaeed460c63b7560a000e3095a56950284c876
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-05-03 13:39:52 -0400

    add resume and import into Navbar. Change navlink hover styling

--- isaiah-collazo/src/components/home/Home.js
+++ isaiah-collazo/src/components/home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import resume from '../../assets/Resume_Isaiah_Collazo.pdf';
 
 const Home = () => {
     return (
--- isaiah-collazo/src/components/navBar/NavBar.js
+++ isaiah-collazo/src/components/navBar/NavBar.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import resume from '../../assets/Resume_Isaiah_Collazo.pdf';
 import '../../css/navBar/navBar.css';
 
 const NavBar = () => {
     return (
         <nav className="mainNav">
             <div className="navLinkContainer">
-                <NavLink exact to="/" className="navLink">HOME</NavLink>
+                <NavLink exact to="/" className="navLink">ME</NavLink>
                 <NavLink exact to="/projects" className="navLink">PROJECTS</NavLink>
+                <a className="navLink" href={resume} target="_blank">RESUME</a>
             </div>
         </nav>
     )
