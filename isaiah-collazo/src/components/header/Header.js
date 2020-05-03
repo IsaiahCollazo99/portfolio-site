31034b60819c438cbf4400b482d55b8b80c25c10
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-05-03 18:49:56 -0400

    set up header css and header component

--- isaiah-collazo/src/components/header/Header.js
+++ isaiah-collazo/src/components/header/Header.js
@@ -6,7 +6,13 @@ const Header = () => {
     return (
         <div className="header">
             <div className="headerLeft">
+                <div className="headerLogo">
 
+                </div>
+                <div className="headerText">
+                    <p className="headerName">Isaiah Collazo</p>
+                    <p className="headerJob">Web Developer</p>
+                </div>
             </div>
 
             <div className="headerRight">
