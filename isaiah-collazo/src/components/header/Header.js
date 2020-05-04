1e6214c9e9d1163827bfc59935a46caef00a78bc
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-05-04 01:28:18 -0400

    add logo to header

--- isaiah-collazo/src/components/header/Header.js
+++ isaiah-collazo/src/components/header/Header.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import NavBar from '../navBar/NavBar';
+import logo from '../../assets/logo.png';
 import '../../css/header/header.css';
 
 const Header = () => {
     return (
         <div className="header">
             <div className="headerLeft">
-                <div className="headerLogo">
-
-                </div>
+                <img src={logo} alt="Logo" className="headerLogo" />
+                
                 <div className="headerText">
                     <p className="headerName">Isaiah Collazo</p>
                     <p className="headerJob">Web Developer</p>
