9947d9d2c31eda284aed160364f5d84b211d3361
Isaiah <isaiahramoncollazo@gmail.com>
2020-05-06 12:22:24 -0400

    change colors to stand out more against background picture

--- isaiah-collazo/src/components/header/Header.js
+++ isaiah-collazo/src/components/header/Header.js
@@ -2,12 +2,13 @@ import React from 'react';
 import NavBar from '../navBar/NavBar';
 import logo from '../../assets/logo.png';
 import '../../css/header/header.css';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div className="header">
             <div className="headerLeft">
-                <img src={logo} alt="Logo" className="headerLogo" />
+                <Link to="/"><img src={logo} alt="Logo" className="headerLogo" /></Link>
                 
                 <div className="headerText">
                     <p className="headerName">Isaiah Collazo</p>
