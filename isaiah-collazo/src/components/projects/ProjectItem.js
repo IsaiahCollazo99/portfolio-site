f1cbe19f072312239292279a299566017b5898b2
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-06-22 22:53:01 -0400

    create ProjectItem component

--- isaiah-collazo/src/components/projects/ProjectItem.js
+++ isaiah-collazo/src/components/projects/ProjectItem.js
@@ -1,9 +1,31 @@
 import React from 'react';
 
 const ProjectItem = ({ project }) => {
+    const {
+        name,
+        description,
+        deploymentUrl,
+        githubRepo,
+        image
+    } = project;
+
     return (
         <div className="projectItem">
+            <div className="projectItemLeft">
+                {/* maybe float left instead of div */}
+                <img src={image} alt={name} />  
+            </div>
 
+            <div className="projectItemRight">
+                <header>
+                    <a href={deploymentUrl ? deploymentUrl : githubRepo}>{name}</a>
+                    {deploymentUrl ? <a href={githubRepo}>Github</a> : <p>Not deployed</p>}
+                </header>
+
+                <p className="projectDesc">
+                    {description}
+                </p>
+            </div>
         </div>
     )
 }
--- isaiah-collazo/src/components/projects/Projects.js
+++ isaiah-collazo/src/components/projects/Projects.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import Project from './ProjectItem';
+import ProjectItem from './ProjectItem';
+import projectsList from '../../util/projectsList';
 
 const Projects = () => {
+    const getProjectComponents = () => {
+        const projectListComponent = [];
+        for(let key in projectsList) {
+            const projectObj = projectsList[key];
+            projectListComponent.push(<ProjectItem project={projectObj} />);
+        }
+        return projectListComponent;
+    }
+    
+    const projectListComponent = getProjectComponents();
+    
     return (
         <div className="projectsPageContainer">
-            projects
+            {projectListComponent}
         </div>
     )
 }
