49b9f123230773c9871363c5fa694e55dc28b604
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-11-05 11:30:10 -0500

    add more svg logos

--- isaiah-collazo/src/util/logos.js
+++ isaiah-collazo/src/util/logos.js
@@ -1,15 +1,15 @@
 import ReactJS from '../assets/logos/React.svg'; 
 import Express from '../assets/logos/ExpressJS.png';
-import NodeJS from '../assets/logos/NodeJS.png';
-import Firebase from '../assets/logos/Firebase.png';
-import SQL from '../assets/logos/Postgresql.png';
+import NodeJS from '../assets/logos/NodeJS.svg';
+import Firebase from '../assets/logos/Firebase.svg';
+import SQL from '../assets/logos/Postgresql.svg';
 import MaterialUI from '../assets/logos/MaterialUI.svg';
 import Javascript from '../assets/logos/JS.svg';
 import HTML from '../assets/logos/HTML.svg';
 import CSS from '../assets/logos/CSS.svg';
 import Redux from '../assets/logos/Redux.svg';
 import Django from '../assets/logos/Django.svg';
-import Python from '../assets/logos/Python.png';
+import Python from '../assets/logos/Python.svg';
 
 const logos = {
     ReactJS,
